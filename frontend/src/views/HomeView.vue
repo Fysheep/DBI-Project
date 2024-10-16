f91b2e912a6d5e3ca1191b838bb2c2aea2d17a6c
Fysheep <121219045+Fysheep@users.noreply.github.com>
2024-10-16 08:02:00 +0200

    Added Home Page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -14,5 +14,11 @@ export default {
 <template>
   <main class="dp-flex fd-c gap-3">
     <h1 consoleheadline class="ta-center">Home</h1>
+    <p class="ta-center">
+      Welcome to the <b> Trackmania Archive</b>, where you can see and edit Sample Data with an underlying MongoDB
+    </p>
+    <p class="ta-center">
+      <img src="../assets/img/image.png" width="500" />
+    </p>
   </main>
 </template>
